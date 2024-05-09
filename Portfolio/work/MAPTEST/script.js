32225efc62771736f93030a5c030a1a0f9da6239
gsymonds1 <oneearth.contant@gmail.com>
2024-05-09 17:59:06 +0100

    mapaddition

--- Portfolio/work/MAPTEST/script.js
+++ Portfolio/work/MAPTEST/script.js
@@ -1,32 +1,41 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0
- */
-// Initialize and add the map
-let map;
-
-async function initMap() {
-  // The location of Uluru
-  const position = { lat: 51.56194305419922, lng: -0.11975682526826859 };
-  // Request needed libraries.
-  //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-  // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
-    zoom: 4,
-    center: position,
-    mapId: "DEMO_MAP_ID",
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: { lat: 51.531240, lng:  -0.116063 },
+    mapTypeId: "satellite",
   });
 
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
-    map: map,
-    position: position,
-    title: "Uluru",
+  map.setTilt(50);
+
+  // Array of marker positions (latitude, longitude pairs)
+  const markerPositions = [
+    { lat: 51.561551, lng: -0.119062 },
+    { lat: 51.515611, lng:  -0.076061 },
+    { lat: 51.536810, lng: -0.125533 },
+    
+  ];
+
+  markerPositions.forEach((position, index) => {
+    const infowindow = new google.maps.InfoWindow({
+      content: `<div id="content">Marker ${index + 1}</div>`+
+      '<p>body</p>'
+      ,
+      ariaLabel: `Marker ${index + 1}`,
+    });
+
+    const marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: `Marker ${index + 1}`,
+    });
+
+    marker.addListener("click", () => {
+      infowindow.open({
+        anchor: marker,
+        map,
+      });
+    });
   });
 }
 
-initMap();
+window.initMap = initMap;
